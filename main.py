238eb4e3d5129ed19f30ddd5644b64e10d812a80
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2025-02-11 20:20:43 -0600

    Add matplotlib. Converted costs to a numpy  array. Created graphs and showed them.

--- main.py
+++ main.py
@@ -14,20 +14,30 @@ acknowledge my commitment to upholding the principles of academic integrity.
 """
 import pprint
 import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
 
-mytickers = ["RKLB", "INTC", "TSLA", "GME", "LCID" ]
+mytickers = ["RKLB", "INTC", "TSLA", "GME", "LCID"]
 mydata = {}
 
+
+
+mytickers.sort()
 for ticker in mytickers:
     result = yf.Ticker(ticker)
-    mydata[ticker] = {'dayHigh': result.info['dayHigh']}
-    # print(f"{ticker} \tDaily High: : {result.info['dayHigh']}")
-
-pprint.pprint(mydata)
-dat = yf.Ticker("MSFT")
-
-
-#pprint.pprint(dat.info)
-
-# hist = dat.history(period="10d")
-# pprint.pprint(hist)
+    hist = result.history(period="10d")
+    last_ten_days = []
+    for date in hist["Close"][:11]:
+        last_ten_days.append(date)
+    if len(last_ten_days) == 10:
+        myarray = np.array(last_ten_days)
+        max_price = myarray.max() + (myarray.max()*.05)
+        min_price = myarray.min() - (myarray.min()*.05)
+        plt.xlabel('Days Ago')
+        plt.ylabel('Closing Price')
+        plt.axis((9, 0, min_price, max_price ))
+        plt.title(f"{ticker} Last 10 Closing Prices")
+        plt.plot(myarray)
+        plt.show()
+    else:
+        print(f"Do not have 10 days of data. Only have {len(last_ten_days)} days")
